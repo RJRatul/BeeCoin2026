2ed6fde160a663eca1c2c8f70cbf29ab0996e206
Rafsan-Ratul <rafsan.ratul.mians@gmail.com>
2026-04-11 15:48:23 +0600

    Fixed cors route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,10 +25,12 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
-  'http://localhost:3002',
   'http://127.0.0.1:3000',
   'http://127.0.0.1:3001',
-  'https://beecoin.cloud'
+  'http://cryptax.live',
+  'https://cryptax.live',
+  'http://www.cryptax.live',
+  'https://www.cryptax.live'
 ];
 
 // Socket.io server
